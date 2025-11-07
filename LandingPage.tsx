2daf16fa03888a491a24dedf56058d024d3982c2
arnav-mani09 <145302787+arnav-mani09@users.noreply.github.com>
2025-11-07 09:12:39 -0800

    making the your personal ai sports betting analyst text be lower to look better on mobile

--- LandingPage.tsx
+++ LandingPage.tsx
@@ -111,7 +111,7 @@ export const LandingPage: React.FC<LandingPageProps> = ({ onLoginSuccess }) => {
           alt="Pick Vision Logo"
           className="w-[300px] md:w-[400px] drop-shadow-[0_0_45px_rgba(168,85,247,0.9)] mb-6 animate-gentleFloat"
         />
-        <p className="text-gray-300 text-lg md:text-xl tracking-wide p-5">
+        <p className="text-gray-300 text-lg md:text-xl tracking-wide p-5 mt-6">
           Your Personal AI Sports Betting Analyst
         </p>
       </div>
