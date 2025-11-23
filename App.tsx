7954f5b0972f197acfb006d8050b3050b178b935
arnav-mani09 <arnavmani01@gmail.com>
2025-11-22 21:15:35 -0800

    fixing statistics

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useCallback, useEffect } from 'react';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { Header } from './components/Header';
 import { ImageUploader } from './components/ImageUploader';
 import { EditableParlayCard } from './components/EditableParlayCard';
